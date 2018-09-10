f0833fb245ffb16b1754b992e85d016c5a9d139c
Ryan Yogan <ryanyogan@gmail.com>
2018-09-10 10:07:42 -0700

    mocked out basics to satisfy the server starting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log('Node started')
+import express from 'express';
+import cors from 'cors';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+const app = express();
+app.use(cors());
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => ({
+      username: 'Ryan Yogan',
+    }),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log(`Apollo Server on http://localhost:8000/graphql`); // eslint-disable-line
+});
