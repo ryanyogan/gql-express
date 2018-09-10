a4643f48c5d17fb8fa83fc9f207a43e6de4c8c7f
Ryan Yogan <ryanyogan@gmail.com>
2018-09-10 10:29:39 -0700

    showing off some basic relationship examples

--- src/index.js
+++ src/index.js
@@ -10,11 +10,22 @@ const schema = gql`
     me: User
     user(id: ID!): User
     users: [User!]
+
+    messages: [Message!]!
+    message(id: ID!): Message!
   }
 
   type User {
     id: ID!
     username: String!
+    messages: [Message!]
+  }
+
+  type Message {
+    id: ID!
+    text: String!
+    userId: ID!
+    user: User!
   }
 `;
 
@@ -22,23 +33,42 @@ const users = {
   1: {
     id: '1',
     username: 'Ryan Yogan',
+    messageIds: [1],
   },
   2: {
     id: '2',
     username: 'Ryan Yogan 2',
+    messageIds: [2],
+  },
+};
+
+const messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'Hello World 2',
+    userId: '2',
   },
 };
 
 const resolvers = {
   Query: {
     me: (_, __, { me }) => me,
-
     user: (parent, { id }) => users[id],
-
     users: () => Object.values(users),
+    messages: () => Object.values(messages),
+    message: (_, { id }) => messages[id],
   },
   User: {
-    username: parent => parent.username,
+    messages: user =>
+      Object.values(messages).filter(message => message.userId === user.id),
+  },
+  Message: {
+    user: message => users[message.userId],
   },
 };
 
